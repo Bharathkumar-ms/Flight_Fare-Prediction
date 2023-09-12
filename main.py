d1b678120cb6845b38ff2c4eea0b4d345f3a1ff1
Bharathkumar M S <96257624+Bharathkumar-ms@users.noreply.github.com>
2023-09-12 10:30:43 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+from src.components.data_ingestion import *
+from src.components.data_cleaning import *
+from src.components.data_transformation import *
+from src.components.model_trainer import *
+from src.components.model_evaluation import *
+        
+if __name__=="__main__":
+    obj=DataIngestion()
+    raw_data_imgestion=obj.initiate_data_ingestion()
+
+    obj2=DataCleaning()
+    raw_arr=obj2.initiate_data_cleaning()
+
+    obj3=DataTransformation()
+    train_arr,test_arr,obj_file_path=obj3.inititate_data_transformation()
+   
+    obj4=ModelTrainer()
+    print(obj4.initiate_model_trainer(train_arr,test_arr))
